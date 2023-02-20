4088f905e8025d618dc5a5eadda5b2b238f42232
Vietchung <vietchungvn@gmail.com>
2023-02-20 19:58:02 +0700

    Post đến file 44

---
+++ TOPIC_PYTHON/TOPIC_TKINTER/b41_ConvertF2Ctem.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showerror
+
+# root window
+root = tk.Tk()
+root.title('Chuyển đổi độ F sang độ C')
+root.geometry('500x90')
+root.resizable(False, False)
+
+
+def fahrenheit_to_celsius(f):
+    """ Convert fahrenheit to celsius
+    """
+    return (f - 32) * 5/9
+
+
+# frame
+frame = ttk.Frame(root)
+
+
+# field options
+options = {'padx': 5, 'pady': 5}
+
+# temperature label
+temperature_label = ttk.Label(frame, text='Độ F')
+temperature_label.grid(column=0, row=0, sticky='W', **options)
+
+# temperature entry
+temperature = tk.StringVar()
+temperature_entry = ttk.Entry(frame, textvariable=temperature)
+temperature_entry.grid(column=1, row=0, **options)
+temperature_entry.focus()
+
+# convert button
+
+
+def convertF2C_button_clicked():
+    """  Handle convert button click event 
+    """
+    try:
+        f = float(temperature.get())
+        c = fahrenheit_to_celsius(f)
+        result = f'{f} F= {c:.2f} oC'
+        result_label.config(text=result)
+    except ValueError as error:
+        showerror(title='Error', message=error)
+
+
+convert_button = ttk.Button(frame, text='Chuyển đổi')
+convert_button.grid(column=2, row=0, sticky='W', **options)
+convert_button.configure(command=convertF2C_button_clicked)
+
+# result label
+result_label = ttk.Label(frame)
+result_label.grid(row=1, columnspan=3, **options)
+
+# add padding to the frame and show it
+frame.grid(padx=0, pady=10)
+
+
+# start the app
+root.mainloop()
--- TOPIC_PYTHON/readme_python.md
+++ TOPIC_PYTHON/readme_python.md
@@ -11,9 +11,11 @@ Bước 1: Clone repositories có địa chỉ:
 https://github.com/aerovfx/Fullstack4kid.git
 sử dụng: CMD, Visual Code, sublimetext, gitbash
 Cú pháp: git clone [địa chỉ]
+
 -Tạo một nhánh con (Branch Tên của mình) sau khi được quản lí add vào nhóm biên tập.
 cú pháp: Git branch [tên nhánh đặt theo tên mình]
 -Soạn thảo code, kiểm thử code
+
 -Chuyển file vừa soạn vào trạng thái chuẩn bị đẩy lên Git
 Cú pháp:
 git add . [. post lên tất cả ]
@@ -23,6 +25,7 @@ git commit -m 'Soạn ghi chú sự thay đổi của code vào đây'
 
 git push origin main [dành cho quản lí]
 git push origin branch [tên nhánh con vừa tạo khi nãy]
+Ví dụ: git push origin baobao_tkinter 
 
 -Kiểm tra trạng thái, kiểm tra sau và trước khi post.
 git status
