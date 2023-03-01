6f26c5340db193e66433c1c674ac9b38802c5fae
Bao Boi <baoboi@Baos-MacBook-Air.local>
2023-03-01 19:21:35 +0700

    bai demo_texteditor

---
+++ TOPIC_PYTHON/TOPIC_DJANGO/Demo_texteditor.py
@@ -0,0 +1,60 @@
+#nhập module tkinter và đặt tên là tk và các module khác khác
+import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+#định nghĩa hàm mở file
+def open_file():
+    """Mở một tập tin để chỉnh sửa."""
+    filepath = askopenfilename(
+        filetypes=[("Text Files", "*.txt"),("All Files", "*.*")]
+    )
+    if not filepath:
+        return
+    txt_edit.delete("1.0", tk.END)
+    with open(filepath, mode="r", encoding="utf-8") as input_file:
+        text = input_file.read()
+        txt_edit.insert(tk.END, text)
+    window.title(f"Simple Text Editor - {filepath}")
+        
+#định nghĩa hàm lưu file
+def save_file():
+    """lưu file hiện tại với tên mới"""
+    filepath = asksaveasfilename(
+        defaultextension=".txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+    )
+    if not filepath:
+        return
+    with open(filepath, mode="w", encoding="utf-8") as output_file:
+        text = txt_edit.get("1.0", tk.END)
+        output_file.write(text)
+    window.title(f"Chương trình biên tập file tẽt đơn giản - {filepath}")
+    
+    
+# tạo cửa sổ chính
+window = tk.Tk()
+window.title("Chương trình biên tập file text đơn giản")
+
+
+# cấu hình hàng và cột của cửa sổ chính
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
+
+#tạo hộp văn bản và khung chứa các nút mở và lưu
+txt_edit = tk.Text(window)
+frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+btn_open = tk.Button(frm_buttons, text="Open", command=open_file)
+btn_save = tk.Button(frm_buttons, text="Save As...", command=save_file)
+
+#đặt các nút mở và lưu vào khung chứa các nút
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+
+
+# đặt khung chứa các nút và hộp văn bản vào cửa sổ chính
+frm_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+
+# gọi phương thức mainloop để giữ cửa sổ hiển thị
+window.mainloop()
