043c00359e4960af2212282b2aa5391bb0aac4b6
Vietchung <vietchungvn@gmail.com>
2023-03-01 18:03:54 +0700

    Ra đề cho bài tập demo số 2

--- TOPIC_PYTHON/TOPIC_TKINTER/Demo_texteditor/readme_texteditor.py
+++ TOPIC_PYTHON/TOPIC_TKINTER/Demo_texteditor/readme_texteditor.py
@@ -1,4 +1,9 @@
+DEMO SỐ 2: TẠO CHƯƠNG TRÌNH MỞ FILE TEXT CHỈNH SỬA NỘI DUNG VÀ LƯU VỚI TÊN MỚI
 Bước 1: import tkinter.
 Bước 2: tạo một cửa sổ mới với tiêu đề "Trình chỉnh sửa văn bản đơn giản".
 Bước 3: thiết lập các cấu hình hàng và cột.
 Bước 4: tạo bốn tiện ích mà bạn sẽ cần cho hộp văn bản, khung, nút mở và nút lưu.
+
+Giao diện app như sau:
+
+<img width="478" alt="Screenshot 2023-03-01 at 18 00 08" src="https://user-images.githubusercontent.com/109997053/222120899-cd5fe353-c377-4bff-98c2-e921864b2399.png">
