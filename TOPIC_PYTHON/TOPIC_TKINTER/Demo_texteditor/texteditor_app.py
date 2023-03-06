3819af40e4f1f19396614fbce0bb3f9627647688
Vietchung <vietchungvn@gmail.com>
2023-03-06 20:03:10 +0700

    Cap nhat lai cac thu muc

--- TOPIC_PYTHON/TOPIC_TKINTER/Demo_texteditor/texteditor_app.py
+++ TOPIC_PYTHON/TOPIC_TKINTER/Demo_texteditor/texteditor_app.py
@@ -43,8 +43,8 @@ window.columnconfigure(1, minsize=800, weight=1)
 #Tạo hộp văn bản và khung chứa các nút mở và lưu
 txt_edit = tk.Text(window)
 frm_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
-btn_open = tk.Button(frm_buttons, text="Open", command=open_file)
-btn_save = tk.Button(frm_buttons, text="Save As...", command=save_file)
+btn_open = tk.Button(frm_buttons, text="Open", fg='black', bg='red', command=open_file)
+btn_save = tk.Button(frm_buttons, fg='black', bg='red', text="Save As...", command=save_file)
 
 #Đặt các nút mở và lưu vào khung chứa các nút
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
