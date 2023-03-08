6e96882c6776f56fe94a359f29c78eaeac85b188
Vietchung <vietchungvn@gmail.com>
2023-03-08 17:36:56 +0700

    Giải phương trình bậc 1

---
+++ TOPIC_PYTHON/TOPIC_TKINTER/Demo_GiaiPTbac1/GiaiPTbac1.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+# Create the main window
+root = tk.Tk()
+root.title("Giải Phương trình bậc 1")
+
+# Create the label and entry widgets for a and b
+a_label = tk.Label(root, text="Nhập vào hệ số a:")
+a_label.grid(row=0, column=0)
+
+a_entry = tk.Entry(root)
+a_entry.grid(row=0, column=1)
+
+b_label = tk.Label(root, text="Nhập vào hệ số b:")
+b_label.grid(row=1, column=0)
+
+b_entry = tk.Entry(root)
+b_entry.grid(row=1, column=1)
+
+# Create the function to solve the equation and display the result
+def solve():
+    a = float(a_entry.get())
+    b = float(b_entry.get())
+    if a == 0:
+        if b == 0:
+            result_label.config(text="phương trình vô số nghiệm (mọi giá trị của x đều thỏa mãn)")
+        else:
+            result_label.config(text="Phương trình vô nghiệm (không có giá trị nào của x thỏa mãn nó)")
+    else:
+        x = -b / a
+        result_label.config(text=f"Nghiệm phương trình là x = {x}")
+
+# Create the Solve button and the label to display the result
+solve_button = tk.Button(root, text="Tính toán", command=solve)
+solve_button.grid(row=2, column=0)
+
+result_label = tk.Label(root, text="")
+result_label.grid(row=2, column=1)
+
+# Run the main event loop
+root.mainloop()
