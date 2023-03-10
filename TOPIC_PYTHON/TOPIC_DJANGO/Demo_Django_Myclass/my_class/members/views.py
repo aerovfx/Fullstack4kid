94d9384e683398dd5d746f810dbdbb1254c92310
Vietchung <vietchungvn@gmail.com>
2023-03-10 17:19:49 +0700

    thêm file all_member.html

--- TOPIC_PYTHON/TOPIC_DJANGO/Demo_Django_Myclass/my_class/members/views.py
+++ TOPIC_PYTHON/TOPIC_DJANGO/Demo_Django_Myclass/my_class/members/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 def members(request):
-  template = loader.get_template('base.html')
-  return HttpResponse(template.render())
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
