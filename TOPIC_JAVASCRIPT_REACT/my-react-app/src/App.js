01c526f77103c11efc0aff653c2c7bbeeb900c24
Bao Boi <baoboi@Baos-MacBook-Air.local>
2023-03-29 19:37:30 +0700

    quay thuoc bao chau

---
+++ TOPIC_JAVASCRIPT_REACT/my-react-app/src/App.js
@@ -0,0 +1,104 @@
+import logo from './logo_nha_thuoc.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1></h1>
+        <p>
+          Aticef,viên nang cứng giá 2500 đồng/viên 
+          Hapenxin capsules,viên nang cứng giá 1900 đồng/viên
+          Hapenxin 250 KIDS,thuốc cốm pha hỗn dịch uống giá 1500/gói
+          Augmentin 1g,viên nén bao phim giá 22.000/viên
+          Klamentin,viên nén dài bao phim giá 8.000/viên
+          Hagimox 250,thuốc bột pha hỗn dịch uống giá 2.000/gói
+          Zinnat tablets 500 mg,viên nén bao phim giá 26.000/viên
+          +Thuốc đông y:
+          Tanakan,viên nén giá 120.000/hộp
+          Thuốc xoang nhất nhất,viên nén bao phim giá 140.000/hộp
+          Dạ dày nhất nhất,viên nén bao phim giá 118.000/hộp
+          Hoạt huyết nhất nhất,viên nén giá 105.000/hộp
+          Đại tràng nhất nhất,viên nén bao phim giá 145.000/hộp
+          Thập toàn đại bổ nhất nhất,viên nén bao phim giá 110.000/hộp
+          Ampelop,viên nang cứng giá 100.000/hộp
+          Phong tê thấp nhất nhất,viên nén bao phim giá 150.000/hộp
+          Tottri,hoàn cứng giá 135.000/hộp
+          Xương khớp nhất nhất,viên nén bao phim giá 95.000/hộp
+          Hoạt huyết CM3,viên nang mềm giá 94.000/hộp
+          Bài thạch danapha,viên nén bao phim giá 50.000/hộp
+          Boganic,viên nang mềm giá 105.000/hộp
+          Boganic,viên bao đường giá 72.000/hộp
+          Phong thấp nam hà giá 30.000/chai
+          Nhiệt miệng PV,viên bao đường giá 75.000/hộp
+          Hoạt huyết dưỡng não,viên nang mềm giá 30.000/hộp
+          Hoạt huyết dưỡng não,viên bao đường giá 15.000/hộp
+          +Thuốc tim mạch:
+          Apitim 5,viên nang cứng giá 24.000/hộp
+          +Thuốc giảm đau,chống viêm không steroid:
+          Mebilax 7.5,viên nén giá 800 đồng/viên
+          Mebilax 15,viên nén giá 1500 đồng/viên
+          Meloxicam tablets BP 7.5mg giá 1.000/viên
+          Alpha choay,viên nén giá 2500/viên
+          +Thuốc chống loét dạ dày:
+          Omeprazol 20 mg,viên nang giá 1.000/viên
+          Omeptul giá 500 đồng/viên
+          Omeprazole STADA 40 mg,viên nang cứng giá 2000 đồng.viên
+          Alzole omeprazol 40 mg,viên nang cứng giá 1200 đồng/viên
+          Helinzole,viên nang cứng giá 1000 đồng/viên
+          Prazopro,viên nang cứng giá 2500 đồng/viên
+          Lansoprazole STELLA 30 mg,viên nang cứng giá 1.500đ/viên
+          Omeprazol DHG,viên nang tan trong ruột giá 1.000/viên
+          Agimoti giá 1500đ/gói
+          Motilium,viên nén giá 2300 đồng/viên
+          Motilium,hỗn dịch uống giá 50.000/chai
+          +Thuốc dùng ngoài:
+          Dibetalic giá 19.000/tuýp
+          Jasunny giá 50.000/tuýp
+          Jasunny giá 5.000/gói
+          -Thuốc không kê đơn:
+          +Vitamin và các chất vô cơ:
+          Beroca,dạng viên sủi giá 70.000/tuýp
+          Calcium corbiere extra giá 180.000/hộp
+          Calcium STELLA,viên nén sủi bọt giá 55.000/tuýp
+          CalSource,viên nén sủi bọt giá 110.000/tuýp
+          Calcium BOSTON,viên nén sủi bọt giá 50.000/tuýp
+          Multivitamin,viên nén bao đường giá 500đ/viên
+          Enervon,viên nén bao phim giá 2200/viên
+          Vitamin B1-B6-B12,viên nén bao phim giá 1000đ/viên
+          Caldihasan,viên nén giá 1.000/viên
+          Calci-D,viên nén dài bao phim giá 450đ/viên
+          Obimin,viên bao phim giá 2500đ/viên
+          Magnesi-B6,viên nén bao phim giá 500đ/viên
+          Magnesi-B6,viên nén bao phim giá 700đ/viên
+          Faincol,viên nén giá 700đ/viên
+          +Thuốc chống dị ứng:
+          Tinfoten 180,viên nang cứng giá 300đ/viên
+          Telfor 60,viên nén bao phim giá 1500đ/viên
+          Telfor 120,viên nén bao phim giá 2600đ/viên
+          Telfast HD 180 mg,viên nén bao phim giá 8000đ/viên
+          Telfast BD 60 mg,viên nén bao phim giá 2500đ/viên
+          Telfast KIDS 30mg,viên nén bao phim giá /viên
+          Robfexo,viên nén bao phim giá 2.000đ/viên
+          Fexophar 120 mg,viên nén bao phim giá 2500đ/viên(đag còn)
+          +Đau và chăm sóc giảm nhẹ:
+          +Đường tiêu hóa:
+          +Hô hấp:
+          +Nhỏ mắt tai,mũi,họng:
+          +Thuốc dùng ngoài:
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
