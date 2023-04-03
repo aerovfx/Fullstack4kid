da6b912d6f3e03a04577471b3be19cad22a64b97
aero <109997053+aerovfx@users.noreply.github.com>
2023-04-03 19:52:19 +0700

    em nop bai hom nay

---
+++ TOPIC_JAVASCRIPT_REACT/my-react-app/src/Appdemo1.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Appdemo1 = () => {
+  return (
+    <div>
+      <h1>Chao mung den voi trang web cua Minh Chau</h1>
+      <h2>Day la trang demo dang ve truyen tranh</h2>
+      <p>Nhung bo truyen moi nhat se duoc dang vao trang nay</p>
+    </div>
+  );
+};
+
+export default Appdemo1;
