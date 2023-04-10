137bbf01b88a2222727b3c3a6e1ecab85a84387a
Vietchung <vietchungvn@gmail.com>
2023-04-10 19:54:08 +0700

    Cap nhat noi dung file Nav va Seacrch

--- TOPIC_JAVASCRIPT_REACT/my-app/src/components/Navbar.jsx
+++ TOPIC_JAVASCRIPT_REACT/my-app/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
 import { logo } from '../utils/constants';
+import SearchBar  from './SearchBar';
 
 const Navbar = () => {
   return (
@@ -10,11 +11,14 @@ const Navbar = () => {
       alignItems="center" 
       p={2} 
       sx={{ position: "sticky", background: '#000', top:0, justifyContent: "space-between" }}> 
-        <Link to="/" style={{ display: 'flex', alignItems: 'center', }}>
-            <img src ={logo} alt="logo" height={45} />
-        </Link>
+      <Link to="/" style={{ display: 'flex', alignItems: 'center', }}>
+        <img src ={logo} alt="logo" height={45} />
+      </Link>
+      <SearchBar />
     </Stack>
+   
   )
+  
 }
 
 export default Navbar
