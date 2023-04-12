47c8c6efc069c919132803cc1cb8e46b5bfc61e0
aero <109997053+aerovfx@users.noreply.github.com>
2023-04-12 19:55:59 +0700

    em nop bai youtube

---
+++ TOPIC_JAVASCRIPT_REACT/youtube_clone/src/components/Feed.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        Sidebar
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright by Minh Chau
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
