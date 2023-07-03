cfd70fbca5d990b3fc5f4d2590f9f5c3ae925fd4
chung dang viet <vietchungvn@gmail.com>
2023-07-03 19:20:08 +0700

    thcs

--- TOPIC_PYTHON/TOPIC_PYTHONBASIC/LUYENTHITHIHSGIOI_THCS/de8/SONTV1.py
+++ TOPIC_PYTHON/TOPIC_PYTHONBASIC/LUYENTHITHIHSGIOI_THCS/de8/SONTV1.py
@@ -1,5 +1,5 @@
-file = open('NGUYENTO.INP')
-file2 = open('NGUYENTO.OUT', 'w')
+file = open('SONT.INP')
+file2 = open('SONT.OUT', 'w')
 def is_prime(n):
     """
     Kiểm tra xem một số có phải là số nguyên tố hay không.
@@ -36,7 +36,7 @@ number_sequence = [2, 3, 5, 7, 2, 3, 7, 11, 2, 5, 11, 11]
 count = count_most_frequent_primes(number_sequence)
 print("Số nguyên tố xuất hiện nhiều nhất:", count)
 
-f_in = open("SONGAY.INP","r"); f_out = open("SONGAY.OUT","w")
+f_in = open("SONT.INP","r"); f_out = open("SONT.OUT","w")
 line_1 = f_in.readline().split()
 line_2 = f_in.readline().split()
 #Tao mang
