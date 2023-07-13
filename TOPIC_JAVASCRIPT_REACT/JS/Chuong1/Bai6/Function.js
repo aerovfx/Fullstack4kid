d0856f48b87d949d486b701796d9e1f54b6a2ce2
vietchung <vietchungvn@gmail.com>
2023-07-13 19:57:00 +0700

    Bai 6

---
+++ TOPIC_JAVASCRIPT_REACT/JS/Chuong1/Bai6/Function.js
@@ -0,0 +1,28 @@
+// function name(parameter1, parameter2, parameter3){
+//     //code
+// }
+
+// function myFunction(a, b){
+//     return a * b;
+// }
+// let c = myFunction(5,10);
+
+// document.write(c);
+
+// const person = {
+//     firstName: "Dang",
+//     lastname: "VietChung",
+//     fullName: function() {
+//         return this.firstName + " " + this.lastname;
+//     }
+// }
+
+// document.write(person.fullName());
+
+// function myFunction() {
+//     alert("This is working")
+// }
+
+hello = (val) => "Hello World!" + val;
+document.write(hello(" Viet Chung"));
+
