9bbd10a03679fda1ef1d0b3fbd4b29aa764b6a57
vietchung <vietchungvn@gmail.com>
2023-07-14 21:43:43 +0700

    React

--- TOPIC_JAVASCRIPT_REACT/base-react-redux/src/redux/reducer/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import counterReducer from './counterReducer';
-
-const rootReducer = combineReducers({
-    counter: counterReducer,
-});
-
-export default rootReducer;
--- TOPIC_JAVASCRIPT_REACT/mern-dashboard-client/src/assets/index.js
+++
@@ -1,4 +0,0 @@
-import logo from "./logo.svg";
-import yariga from "./yariga.svg";
-
-export { logo, yariga };
