2e11a264a1a970c3d146c6a53de2242c67fdd24f
aero <109997053+aerovfx@users.noreply.github.com>
2023-07-19 20:57:47 +0700

    Delete CREATE_APP/python/PythonStudy/boiboistudy directory
    Signed-off-by: aero <109997053+aerovfx@users.noreply.github.com>

--- CREATE_APP/python/PythonStudy/boiboistudy/b52.py
+++
@@ -1,8 +0,0 @@
-numbers = input('Nhap so phan tu cua day:')
-numbers = numbers.split(',')
-odd_numbers = []
-tong=[]
-for num in numbers:
-    num = int(num)
-    if num % 2 ==1 : tong += num 
-print('Tong cac so le trong day la:', tong)
