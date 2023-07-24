b1b2e82df9b52976f3c487c48bd855f2af8f2207
unknown <nguyenhoangminhchau02122009@gmail.com>
2023-07-24 18:32:11 +0700

    bai 9

--- CREATE_APP/Python/PythonStudy/BoiBoistudy/Python_basic/bai9.py
+++ CREATE_APP/Python/PythonStudy/BoiBoistudy/Python_basic/bai9.py
@@ -1,7 +1,9 @@
-n = int(input("Nhập số cần tính giai thừa: "))
+#Nhập số n
+n = int(input("Nhập số cần tính giai thừa: "))  
+#Hàm tính giai thừa
 def giai_thua(n):
     if n == 0:
         return 1
     return n *giai_thua(n - 1)
- 
+#In ra kết quả
 print (giai_thua(n))
