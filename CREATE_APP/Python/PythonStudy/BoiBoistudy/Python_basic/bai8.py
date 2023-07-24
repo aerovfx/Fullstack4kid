a280dc095c1b326d25cc149c6bc2ff6aa8249b5a
unknown <nguyenhoangminhchau02122009@gmail.com>
2023-07-24 18:36:16 +0700

    bai 8

--- CREATE_APP/Python/PythonStudy/BoiBoistudy/Python_basic/bai8.py
+++ CREATE_APP/Python/PythonStudy/BoiBoistudy/Python_basic/bai8.py
@@ -1,3 +1,6 @@
-for i in range(2000, 3200):  #Cho i chạy từ 2000 đến 3200
-    if i % 7 == 0 and i%5==1:  #Tìm các số chia hết cho 7 nhưng không phải bội của số 5
-        print (i)  #In ra kết quả
+#Cho i chạy từ 2000 đến 3200
+for i in range(2000, 3200):  
+#Tìm các số chia hết cho 7 nhưng không phải bội của số 5   
+    if i % 7 == 0 and i%5==1:      
+#In ra kết quả
+        print (i) 
