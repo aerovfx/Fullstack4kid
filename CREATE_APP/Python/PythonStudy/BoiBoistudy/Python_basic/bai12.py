32c662f1b9262d103243f1323aac5716b4ce38c9
unknown <nguyenhoangminhchau02122009@gmail.com>
2023-07-28 19:10:17 +0700

    bai 12

--- CREATE_APP/Python/PythonStudy/BoiBoistudy/Python_basic/bai12.py
+++ CREATE_APP/Python/PythonStudy/BoiBoistudy/Python_basic/bai12.py
@@ -1,8 +1,14 @@
 import math
-# Đọc dữ liệu từ file "BAI12":
 def can_bac_hai(danh_sach):
     a = math.sqrt(danh_sach)
     return a
 
-n=int(input('Nhập vào 1 số:'))
-print(can_bac_hai(n))
+# Đọc dữ liệu từ file "BAI12":
+with open('BAI12.INP', 'r') as file:
+    danh_sach = list(map(int, file.readline().split(",")))
+for i in range(1, danh_sach):
+    
+    
+#Xuất dữ liệu ra file "BAI12":
+with open('BAI12.OUT','w') as file:
+    file.write(str(danh_sach))
