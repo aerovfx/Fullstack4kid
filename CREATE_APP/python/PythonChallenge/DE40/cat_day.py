824f2d6b9bb037d763867cc493bd4f8f57fde992
comfortchain <124359029+comfortchain@users.noreply.github.com>
2023-08-11 19:04:00 +0700

    Update cat_day.py

--- CREATE_APP/python/PythonChallenge/DE40/cat_day.py
+++ CREATE_APP/python/PythonChallenge/DE40/cat_day.py
@@ -1,12 +1,3 @@
-#Hàm:
-def tinh_tong(n):
-    s = 0
-s = n 
-    temp = n
-#Đọc n,k vào file INP:
-with open('SONGUYENV2.INP','r') as file:
-    n,k = map(int(file.readline().split())
-  
-            
+
 
             
