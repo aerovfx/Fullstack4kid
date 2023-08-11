ef0be7401a923cddd1d9ca7ff5899d218d7dcd85
comfortchain <124359029+comfortchain@users.noreply.github.com>
2023-08-11 19:31:02 +0700

    Update .py

--- CREATE_APP/python/PythonChallenge/DE40/cat_day.py
+++ CREATE_APP/python/PythonChallenge/DE40/cat_day.py
@@ -1,3 +1,19 @@
-
+with open('SONGUYEN.INP','r') as file:
+    n,k = map(int, file.readline().split())
+def tinh_tong(n):   
+    s = n
+    temp = n
+    for i in range(k):
+           temp = temp * 10
+           s += temp
+    return s
+s = tinh_tong(n)
+sum = 0
+s = str(s)
+for i in range(len(s)):
+    sum += int(s[i])
+tong = tinh_tong(s)
+with open('SONGUYEN.OUT','w') as file:
+    file.write(str(sum))
 
             
