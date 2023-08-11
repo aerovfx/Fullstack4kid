6b16194dcce79474c1fb572632ed29bfcf4e8629
comfortchain <124359029+comfortchain@users.noreply.github.com>
2023-08-11 18:52:58 +0700

    Update songuyenv2.py

--- CREATE_APP/python/PythonChallenge/DE40/cat_day.py
+++ CREATE_APP/python/PythonChallenge/DE40/cat_day.py
@@ -1,13 +1,4 @@
-with open('cat_day.INP', 'r') as file:
-    n = int(file.read())
-    count = 0
-    for x in range(1, n):
-        y = n-2*x
-        if (x+x>y) and (y>x): 
-            count +=1
-            print(x,y)
-with open('cat_day.OUT','w') as file:
-    file.write(str(count))
+
             
 
             
