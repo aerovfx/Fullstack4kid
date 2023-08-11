fdf12c11e670a253568919eaa8d20ff81deb068c
comfortchain <124359029+comfortchain@users.noreply.github.com>
2023-08-11 18:30:34 +0700

    Update xoa.py

--- CREATE_APP/python/PythonChallenge/DE40/xoa.py
+++ CREATE_APP/python/PythonChallenge/DE40/xoa.py
@@ -1,2 +1,13 @@
-danh_sach = [4,  3, 7, 9, 4, 5]
-print(set(danh_sach))
+with open('SONGUYEN.INP','r') as file:
+    n,k = int(file.readline().strip().split())
+    s = n
+    temp = n
+    for i in range(k):
+        temp = temp * 10
+        s += temp
+ 
+    sum = 0
+    for i in range(len(s)):
+        sum += int(s[i])
+with open('SONGUYEN.OUT','w') as file:
+    file.write(str(sum))
