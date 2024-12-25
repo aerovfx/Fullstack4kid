3c2e99a430a9b4a24a32d64f23a96f2b28745df8
chung dang viet <chungdangviet@192.168.1.3>
2024-12-25 22:23:43 +0700

    up file data

--- CREATE_APP/Cplus/Study/cplus_data.cpp
+++ CREATE_APP/Cplus/Study/cplus_data.cpp
@@ -4,3 +4,22 @@ int a = 10; //Biến nguyên
 float b = 3.14; // Biến số thực
 char c = 'A'; // Biến ký tự
 std::string chuoi = "Hello"; //Biến chuỗi
+bool d = true; // Biến logic
+
+//Hằng
+const float PI = 3.14159; // Hằng
+
+// TOÁN TỬ
+//Toán tử số học
+int x = 10, y = 3;
+int tong = x + y;
+int hieu = x - y;
+int tich = x * y;
+int thuong = x/y;
+int du = x%y;
+
+//Toán tử so sánh
+bool kq = (x> y); //true
+
+//Toán tử logic
+bool kq = (x > 5 && y < 10); //True
