e5d1a0aa21a2ef2acf27dc9d2aa246ca4ac98092
chung dang viet <chungdangviet@192.168.1.4>
2025-01-01 16:44:46 +0700

    up

---
+++ CREATE_APP/C/Study/c_hammain.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Thư viện chuẩn hỗ trợ các hàm nhập/xuất như printf
+
+int main() { // Hàm chính, nơi chương trình bắt đầu thực thi
+    printf("Hello World"); // In chuỗi ký tự "Hello World" ra màn hình
+    return 0; // Trả về giá trị 0, báo hiệu chương trình kết thúc thành công
+}
